920d6b0f396a5732b1b9841a8c2f0224c7f53417
Gorakshnath <gorakshnath107@gmail.com>
2023-06-21 09:36:26 +0530

    adding some styling

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,33 +1,74 @@
-import React from 'react'
-import '../stylesheets/nav.css'
-import {
-  NavLink
-} from "react-router-dom";
+import React from "react";
+import "../stylesheets/nav.css";
+import { NavLink } from "react-router-dom";
 function Navigator() {
   return (
     <div>
-      <nav style={{    backgroundColor: 'burlywood'}} className=" navbar navbar-expand-lg bg-body-primary">
-  <div className="container-fluid">
-    <b className="navbar-brand">TAAZA KHABAR</b>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item"><NavLink className="nav-link " aria-current="page" to='/'>Home</NavLink></li>
-        <li className="nav-item"><NavLink className="nav-link" to='/business' >Business</NavLink></li>
-        <li className="nav-item"><NavLink className="nav-link" to='/entertainment'>Entertainment</NavLink></li>
-        <li className="nav-item"><NavLink className="nav-link " to='/general'>General</NavLink></li>
-        <li className="nav-item"><NavLink className="nav-link " to='/health'>Health</NavLink></li>
-        <li className="nav-item"><NavLink className="nav-link " to='/science' >Science</NavLink></li>
-        <li className="nav-item"><NavLink className="nav-link " to='/sports'>Sports</NavLink></li>
-        <li className="nav-item"><NavLink className="nav-link " to='/technology'>Technology</NavLink></li>
-      </ul>
+      <nav
+        style={{ backgroundColor: "burlywood" }}
+        className=" navbar navbar-expand-lg bg-body-primary"
+      >
+        <div className="container-fluid">
+          <b className="navbar-brand">TAAZA KHABAR</b>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link " aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/business">
+                  Business
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/entertainment">
+                  Entertainment
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link " to="/general">
+                  General
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link " to="/health">
+                  Health
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link " to="/science">
+                  Science
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link " to="/sports">
+                  Sports
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link " to="/technology">
+                  Technology
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-    </div>
-  )
+  );
 }
 
-export default Navigator
+export default Navigator;
--- src/components/top-headlines/NewsSection.js
+++ src/components/top-headlines/NewsSection.js
@@ -1,77 +1,84 @@
-import React, { useEffect, useState } from 'react'
-import InfiniteScroll from 'react-infinite-scroll-component';
-import Template from './Template'
-import './style.css';
-import Loader from '../Loader';
-import notFoundImage from './notfound.jpeg'
-
+import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Template from "./Template";
+import "./style.css";
+import Loader from "../Loader";
+import notFoundImage from "./notfound.jpeg";
 
 function NewsSection(props) {
-  const [arts,setArticles] = useState([])
-  const [totalResults,setTR] = useState(0)
-  const[page,setPage] = useState(1)
-  const [loader,setLoader] = useState(true)
-  useEffect(()=>{
-
+  const [arts, setArticles] = useState([]);
+  const [totalResults, setTR] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
     getData();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
-  
-  const getData =   () =>{
-    setLoader(true)
-  fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&page=${page}&pageSize=25&apiKey=${'1f8e293a9f6344589a5276fe1a768680'}`)
-  .then(response => response.json())
-  .then((data) => {
-        setArticles(data.articles)
-    setTR(data.totalResults)
-    setLoader(false)
-  });
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  const getData = () => {
+    setLoader(true);
+    fetch(
+      `https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&page=${page}&pageSize=25&apiKey=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setArticles(data.articles);
+        setTR(data.totalResults);
+        setLoader(false);
+      });
+  };
 
   // FETCH MORE DATA METHOD
- const fetchMoreData = async () => {
-     setPage((page) => page + 1);
-     fetch(`https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&page=${page}&pageSize=20&apiKey=${'1f8e293a9f6344589a5276fe1a768680'}`)
-     .then(response => response.json())
-     .then((data) => {
-      setArticles(arts.concat(data.articles))
-       setTR(data.totalResults)
-
-     });
+  const fetchMoreData = async () => {
+    setPage((page) => page + 1);
+    fetch(
+      `https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&page=${page}&pageSize=20&apiKey=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setArticles(arts.concat(data.articles));
+        setTR(data.totalResults);
+      });
   };
   return (
     <>
-    {
-    !loader?
-      <div>
-        <div className='mt-4 container-fluid mb-4'>
-{arts.map((items)=>{
-       return (
-        <div className='card' key={crypto.randomUUID()} >
-      <Template
-       imageUrl={items.urlToImage?items.urlToImage:notFoundImage}
-        title={items.title?items.title.slice(0,60):""}
-         desc={items.description?items.description.slice(0,100):""}
-          lnk={items.url}
-          auth={items.source.name}
-          date={items.publishedAt?items.publishedAt.slice(0,10):''}
-          />
+      {!loader ? (
+        <div>
+          <div className="mt-4 container-fluid mb-4">
+            {arts.map((items) => {
+              return (
+                <div className="card" key={crypto.randomUUID()}>
+                  <Template
+                    imageUrl={
+                      items.urlToImage ? items.urlToImage : notFoundImage
+                    }
+                    title={items.title ? items.title.slice(0, 60) : ""}
+                    desc={
+                      items.description ? items.description.slice(0, 100) : ""
+                    }
+                    lnk={items.url}
+                    auth={items.source.name}
+                    date={
+                      items.publishedAt ? items.publishedAt.slice(0, 10) : ""
+                    }
+                  />
+                </div>
+              );
+            })}
+            <InfiniteScroll
+              dataLength={arts.length}
+              next={fetchMoreData}
+              hasMore={arts.length !== totalResults}
+            ></InfiniteScroll>
+          </div>
         </div>
-     );})}
-               <InfiniteScroll
-          dataLength={arts.length}
-          next={fetchMoreData}
-          hasMore={arts.length !== totalResults}
-        ></InfiniteScroll>
-     </div>
-     </div>
-     :<Loader/>
-}
+      ) : (
+        <Loader />
+      )}
     </>
-  )
+  );
 }
 NewsSection.defaultProps = {
-  category:'general'
-}
+  category: "general",
+};
 export default NewsSection;
